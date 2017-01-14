ce5ada73ec88c4c188b69b26d8b385be0451f792
panpanick <claud@openmailbox.org>
2017-01-14 00:01:30 -0500

    basic bullets working

---
+++ unityProject/TotallyNotCyberspaceDoom/Assets/Scripts/deactivator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deactivator : MonoBehaviour {
+
+	public float timeTillDeactivate;
+
+	void OnEnable() {
+		StartCoroutine(CountDown());
+	}
+
+	IEnumerator CountDown() {
+		yield return new WaitForSecondsRealtime(timeTillDeactivate);
+		gameObject.SetActive(false);
+	}
+}
--- unityProject/TotallyNotCyberspaceDoom/Assets/Scripts/gun.cs
+++ unityProject/TotallyNotCyberspaceDoom/Assets/Scripts/gun.cs
@@ -11,12 +11,70 @@ public class gun : MonoBehaviour {
 	}
 
 	Animator anim;
+	public Transform muzzleOpening;
+	public float bulletForce;
+
+	public GameObject bullet;
+	public GameObject[] bullets;
+	public int pooledBullets;
+	List<GameObject> bulletList;
+
+
 
 	void Awake() {
 		anim = GetComponent<Animator>();
+
+		PoolBullets();
 	}
 
 	void Update() {
-		if (Input.GetButton("Fire1")) anim.SetTrigger("fire");
+		if (Input.GetButton("Fire1")) {
+			anim.SetTrigger("fire");
+			GrabBullet();
+		}
+	}
+
+	void PoolBullets() {
+		bulletList = new List<GameObject>();
+		for (int i = 0; i < pooledBullets; ++i) {
+			GameObject pomf = Instantiate(bullets[0]) as GameObject;
+			GameObject desu = Instantiate(bullets[1]) as GameObject;
+			GameObject loli = Instantiate(bullets[2]) as GameObject;
+			GameObject nani = Instantiate(bullets[3]) as GameObject;
+			GameObject onii = Instantiate(bullets[4]) as GameObject;
+			GameObject chan = Instantiate(bullets[5]) as GameObject;
+
+			pomf.SetActive(false);
+			desu.SetActive(false);
+			loli.SetActive(false);
+			nani.SetActive(false);
+			onii.SetActive(false);
+			chan.SetActive(false);
+
+			bulletList.Add(pomf);
+			bulletList.Add(desu);
+			bulletList.Add(loli);
+			bulletList.Add(nani);
+			bulletList.Add(onii);
+			bulletList.Add(chan);
+		}
+	}
+
+	void GrabBullet() {
+		for (int i = 0; i < bulletList.Count; ++i) {
+			if (bulletList[i] != null && !bulletList[i].activeInHierarchy) {
+				bulletList[i].transform.position = muzzleOpening.position;
+				bulletList[i].transform.rotation = muzzleOpening.rotation;
+				bulletList[i].GetComponent<deactivator>().timeTillDeactivate = 3;
+				bulletList[i].SetActive(true);
+
+				//fire
+				Rigidbody fam = bulletList[i].GetComponent<Rigidbody>();
+				fam.AddForce(-muzzleOpening.forward * bulletForce);
+
+
+				break;
+			}
+		}
 	}
 }
