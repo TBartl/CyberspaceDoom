0d18fa3245a07b406586c244704d40c604503cb9
TBartl <tbartl@umich.edu>
2017-01-14 18:55:13 -0500

    Jumper enemy

---
+++ unityProject/TotallyNotCyberspaceDoom/Assets/Entities/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	protected Rigidbody rigid;
+	protected Transform targetPlayer;
+	public float aggroRadius = 10f;
+	bool chasing = false;
+
+	// Use this for initialization
+	void Start() {
+		rigid = GetComponent<Rigidbody>();
+		targetPlayer = GameObject.Find("player").transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		CheckPlayerClose();
+	}
+
+	void CheckPlayerClose() {
+		if (chasing != InRangeOfPlayer())
+			chasing = !chasing;
+		else {
+			ChasePlayer();
+		}
+	}
+	
+	protected virtual void ChasePlayer() {
+	}
+
+	bool InRangeOfPlayer() {
+		return Vector3.Distance(transform.position, targetPlayer.position) <= aggroRadius;
+	}
+}
---
+++ unityProject/TotallyNotCyberspaceDoom/Assets/Entities/Jumper/JumperEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumperEnemy : Enemy {
+	public float maxTimeToJump = 2f;
+	public float jumpStrength = 10f;
+	bool canJump = true;
+	Animator anim;
+
+	void Awake() {
+		anim = this.GetComponent<Animator>();
+	}
+
+	protected override void ChasePlayer() {
+		anim.SetFloat("Velocity", Vector3.Dot(rigid.velocity.normalized, transform.up));
+		if (canJump) {
+			Vector3 direction = Vector3.zero;
+			direction += Vector3.up * 2f;
+			direction += (targetPlayer.position - this.transform.position).normalized;
+			direction = direction.normalized;
+			rigid.velocity = direction * jumpStrength;
+			transform.rotation = Quaternion.LookRotation((targetPlayer.position - transform.position).normalized, transform.position.normalized);
+			anim.SetBool("Jumping", true);
+			StartCoroutine(JustJumped());
+		}
+	}
+
+	IEnumerator JustJumped() {
+		canJump = false;
+		yield return new WaitForSeconds(maxTimeToJump);
+		canJump = true;
+	}
+}
