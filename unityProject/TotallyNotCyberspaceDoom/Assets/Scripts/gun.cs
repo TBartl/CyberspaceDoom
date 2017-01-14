052ff55b840390f4369a3a14b7cee75f0c1eff0d
panpanick <claud@openmailbox.org>
2017-01-13 21:20:55 -0500

    finished gun animations

---
+++ unityProject/TotallyNotCyberspaceDoom/Assets/Scripts/gun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour {
+
+	public enum modes {
+		pistol,
+		shotgun,
+		lazer
+	}
+
+	Animator anim;
+
+	void Awake() {
+		anim = GetComponent<Animator>();
+	}
+
+	void Update() {
+		if (Input.GetButton("Fire1")) anim.SetTrigger("fire");
+	}
+}
