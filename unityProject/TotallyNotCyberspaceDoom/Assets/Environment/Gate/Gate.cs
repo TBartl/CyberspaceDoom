724436e955ce124632ea03e198b9e75b14a7f2ce
TBartl <tbartl@umich.edu>
2017-01-14 23:08:49 -0500

    wdy:w

--- unityProject/TotallyNotCyberspaceDoom/Assets/Environment/Gate/Gate.cs
+++ unityProject/TotallyNotCyberspaceDoom/Assets/Environment/Gate/Gate.cs
@@ -3,14 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gate : MonoBehaviour {
+	public Material litMaterial;
+	MeshRenderer mr;
+	List<Monolith> monoliths;
+	public static Gate s;
+	int monolithsActivated = 0;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Awake() {
+		mr = this.GetComponent<MeshRenderer>();
+		s = this;
+
+		monoliths = new List<Monolith>();
+		List<GameObject> monolithGOs = new List<GameObject>(GameObject.FindGameObjectsWithTag("Monolith"));
+		foreach (GameObject monolith in monolithGOs) {
+			monoliths.Add(monolith.GetComponent<Monolith>());
+		}
+		while (monoliths.Count > 2) {
+			Monolith m = monoliths[Random.Range(0,monoliths.Count)];
+			monoliths.Remove(m);
+			Destroy(m.gameObject);
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	// Debug key in the literal worst possible place because I'm lazy
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.P)) {
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false; ;
+		}
+	}
+
+	public void MonolithActivated() {
+		Light(monolithsActivated);
+		monolithsActivated += 1;
+		if (monolithsActivated == 2)
+			transform.GetChild(0).gameObject.SetActive(true);
+
+	}
+
+	void Light(int i) {
+		Material[] mats = mr.materials;
+		if (i == 0)
+			mats[2] = litMaterial;
+		else if (i == 1)
+			mats[3] = litMaterial;
+		mr.materials = mats;
+
 	}
 }
