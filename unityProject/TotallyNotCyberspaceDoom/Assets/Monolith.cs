88ebfb0e3bc9e6b824f27ebc48e703acb9d6afca
TBartl <tbartl@umich.edu>
2017-01-15 17:43:38 -0500

    Hrmm

--- unityProject/TotallyNotCyberspaceDoom/Assets/Monolith.cs
+++ unityProject/TotallyNotCyberspaceDoom/Assets/Monolith.cs
@@ -6,21 +6,52 @@ public class Monolith : MonoBehaviour {
 
 	bool monolithActivated = false;
 	MeshRenderer mr;
+	public float spawnRate = 14;
+	public float increasedSpawnRate = 4;
+	bool useIncreasedSpawn = false;
+	public List<GameObject> enemies;
 
 	void Awake() {
 		mr = this.GetComponent<MeshRenderer>();
+		StartCoroutine(CountDownToIncreased());
+		StartCoroutine(SpawnEnemies());
+
 	}
 
 	void OnCollisionEnter(Collision coll) {
 
 		if (!monolithActivated &&
-			(coll.collider.tag == "Player" || coll.collider.tag == "Bullet") && 
+			(coll.collider.tag == "Player" || coll.collider.tag == "Bullet") &&
 			transform.childCount == 0) {
 
-			Debug.Log("Doot");
 			mr.material = mr.materials[1];
 			Gate.s.MonolithActivated();
 			monolithActivated = true;
 		}
+	}
+
+	IEnumerator SpawnEnemies() {
+		while (true) {
+			SpawnRandomEnemy();
+			yield return new WaitForSeconds(GetSpawnRate());
+		}
+	}
+
+	void SpawnRandomEnemy() {
+		int enemyIndex = Random.Range(0, enemies.Count);
+		Vector2 circle = Random.insideUnitCircle * 10f;
+		Vector3 position = this.transform.position + transform.TransformVector(Vector3.up * 10f + new Vector3(circle.x, 0, circle.y));
+		Instantiate(enemies[enemyIndex], position, Quaternion.identity);
+	}
+
+	IEnumerator CountDownToIncreased() {
+		yield return new WaitForSeconds(60);
+	}
+
+	float GetSpawnRate() {
+		if (useIncreasedSpawn)
+			return increasedSpawnRate;
+		else
+			return spawnRate;
 	}
 }
