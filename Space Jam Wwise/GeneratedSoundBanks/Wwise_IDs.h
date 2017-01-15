d79def070404721210d6cdf56b5aa3dc16216da3
PHJurasek <PeteJurasek@0587396338.wireless.umich.net>
2017-01-14 23:43:58 -0500

    hopefully this works

---
+++ Space Jam Wwise/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,83 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID FOOTSTEP = 1866025847U;
+        static const AkUniqueID ICEAMBIENCE = 1952003650U;
+        static const AkUniqueID LASER = 3982605422U;
+        static const AkUniqueID PISTOL = 324443136U;
+        static const AkUniqueID RAINAMBIENCE = 3812882407U;
+        static const AkUniqueID SHADOWAMBIENCE = 693161857U;
+        static const AkUniqueID SHOTGUN = 51683977U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace STATES
+        {
+            static const AkUniqueID GROUP = 1208172739U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ICEPLANET = 3892807504U;
+                static const AkUniqueID RAINPLANET = 1776102009U;
+                static const AkUniqueID SHADOWPLANET = 3354777067U;
+            } // namespace STATE
+        } // namespace STATES
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace AMBIENCES
+        {
+            static const AkUniqueID GROUP = 1017660616U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ICE = 344481046U;
+                static const AkUniqueID RAIN = 2043403999U;
+                static const AkUniqueID SHADOW = 3140781661U;
+            } // namespace SWITCH
+        } // namespace AMBIENCES
+
+        namespace FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 2385628198U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ICE = 344481046U;
+                static const AkUniqueID RAIN = 2043403999U;
+                static const AkUniqueID SHADOW = 3140781661U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAINSOUNDBANK = 534561221U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
